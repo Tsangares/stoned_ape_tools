826188b1aab8e69d84e6c70e89580d2efb252298
William Wyatt <tsangares@gmail.com>
2023-02-12 19:12:34 -0800

    Changed var to let

--- source/intel.js
+++ source/intel.js
@@ -584,7 +584,7 @@ function Legacy_NeptunesPrideAgent() {
           arrival.getMinutes(),
         )}`;
     } else {
-      var totalETA = arrival - now;
+      let totalETA = arrival - now;
       ttt = p + Crux.formatTime(totalETA);
     }
     return ttt;
